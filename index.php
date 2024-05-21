81e3f535373ebc38a0dfa0609ec9c7aeb47e369c
kayleigh_monaghan <kayleigh.monaghan@icloud.com>
2024-05-21 14:20:49 +0100

    styling

--- index.php
+++ index.php
@@ -3,26 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculator</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <!-- action points towards php inside this doc rather than pointing 
-    to an external php file -->
-    <form action="<?php echo htmlspecialchars($_SERVER
-    ["PHP_SELF"]); ?>" method="post">
-        <input type="number" name="num01" placeholder="number one" />
-        <select name="operator">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" name="num02" placeholder="number two" />
-        <button>Calculate</button>
-    </form>
-
+    <h1>Calculator</h1>
+    <div class="container">
+        <!-- action points towards php inside this doc rather than pointing 
+        to an external php file -->
+        <form action="<?php echo htmlspecialchars($_SERVER
+        ["PHP_SELF"]); ?>" method="post">
+            <input type="number" name="num01" placeholder="number input" required />
+            <select name="operator">
+                <option value="add">+</option>
+                <option value="subtract">-</option>
+                <option value="multiply">*</option>
+                <option value="divide">/</option>
+            </select>
+            <input type="number" name="num02" placeholder="number input" required />
+            <button>calculate</button>
+        </form>
+    </div>
+    <footer>
+        <p>Created by <a href="https://github.com/kayleighmonaghan">Kayleigh Monaghan</a>👩‍💻</p>
+    </footer>
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // fetch data from inputs
@@ -38,19 +43,19 @@
             $errors = false;
 
             if (empty($num01) || empty($num02) || empty($operator)) {
-                echo "<p>Please fill in all fields.</p>";
+                echo "<p class='error-msg'>Please fill in all fields.</p>";
                 $errors = true;
             }
 
             if (!is_numeric($num01) || !is_numeric($num02)) {
-                echo "<p>Only write numbers.</p>";
+                echo "<p class='error-msg'>Only write numbers, silly!</p>";
                 $errors = true;
             }
 
             // if no errors: calculate numbers
             if (!$errors) {
                 $value = 0;
-                
+
                 switch ($operator) {
                     case "add":
                         $value = $num01 + $num02;
@@ -68,7 +73,8 @@
                         echo "<p>Uh oh! Something went wrong o_O</p>";
                 }
 
-                echo "<p>Result = " . $value . "</p>";
+                echo "<p class='result'>result = " . $value . "</p>";
+                echo "<button class='reset-btn'><a href='index.php'>reset</a></button";
             }
         }
     ?>
